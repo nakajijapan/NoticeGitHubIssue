39bbb8825bdc98f3c362e95fd86d83d3d4e64d79
nakajijapan <nakajima@paperboy.co.jp>
2012-11-05 14:27:21 +0900

    for ruby 1.9.3p286

--- feeds.rb
+++ feeds.rb
@@ -4,6 +4,23 @@ require 'open-uri'
 require 'simple-rss'
 require 'active_support/all'
 
+
+#---------------------------------------
+# a painful modification
+#---------------------------------------
+class SimpleRSS_CSTM < SimpleRSS
+  def unescape(content)
+    if content =~ /([^-_.!~*'()a-zA-Z\d;\/?:@&=+$,\[\]]%)/u then
+      CGI.unescape(content).gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
+    else
+      content.gsub(/(<!\[CDATA\[|\]\]>)/,'').strip
+    end
+  end
+end
+
+#---------------------------------------
+# main
+#---------------------------------------
 load File::expand_path('', File::dirname(__FILE__)) + '/config.rb'
 
 if ARGV[0].nil?
@@ -14,20 +31,27 @@ organization = ARGV[0]
 
 feed_url = "https://github.com/organizations/#{organization}/#{$USERNAME}.private.atom?token=#{$USER_TOKEN}"
 
-rss = SimpleRSS.parse open(feed_url)
+p feed_url
+
+rss = SimpleRSS_CSTM.parse open(feed_url)
 
 rss.items.each do |item|
   next if item.title =~ /pushed/
-  now = Time.now
 
+  # init
+  now = Time.now
   updated = item.updated.getlocal
-  #p "#{updated} > #{5.minutes.ago(now)}"
+  regex_content = /blockquote&gt;(.+)&lt;\/blockquote/
+
   #p "[#{updated}] #{item.title}"
-  if item.updated > now - 6.minutes
-    p "[#{updated}] #{item.title}"
+  if item.updated > now - 5.minutes
+    content = ''
+    contents = item.content.gsub(/\n/, '').scan(regex_content)
+    content = contents.pop.pop.strip unless contents.empty?
 
     # via ikachan
     system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"GitHub  #{item.title} \" #{$IRC_URL}")
+    system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"GitHub  #{content}  \" #{$IRC_URL}") if content.present?
     system("curl -F channel=\##{$IRC_CHANNEL} -F message=\"GitHub  #{item.link}  \" #{$IRC_URL}")
   end
 end
